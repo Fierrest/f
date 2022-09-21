697cfcf6de056bdbfa0ae11307deb291a10a935f
Fierrest <Fierresteyliem@gmail.com>
2022-09-21 17:49:11 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>index</title>
+
+</head>
+<body>
+    <h1>halo selamat datang di resume saya</h1>
+    <p><?php echo date ('d/m/Y'); ?><?php
+Route::get ('/portfolio')?>
+    <h2>berikut adalah data diri saya</h2>
+    <h2>Nama:Fierrest Eyliem</h2>
+    <h2>Tempat,tanggal lahir:Riau,28 juli 2002</h2>
+<h2>Umur:20</h2>
+<h2>Hobi:makan</h2>
+<div class ="container mt-5">
+    <img src="/images/anakayam.jpg"width="100%"/>
+    </div>
+
+
+    </div>
+</body>
+</html>
---
+++ resources/views/portofolio.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>profile</title>
+        <link rel="stylesheet" href="/css/style.css"
+</head>
+<body>
+
+<h1>Ini adalah portofolio saya</h1>
+<div class ="container mt-5">
+    <img src="/images/photo portofolio.PNG"width="100%"/>
+    </div>
+</body>
+</html>
